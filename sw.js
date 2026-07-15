ccf8418c821ce7df348310defb16a231571b77a9
philliplock-stack <philliplock@gmail.com>
2026-07-15 10:30:48 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'donegal-birds-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/app.js',
+  '/manifest.json',
+  '/assets/chough.jpg',
+  '/assets/chough.mp3',
+  '/assets/corncrake.jpg',
+  '/assets/corncrake.mp3',
+  '/assets/icon-192.png',
+  '/assets/icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
